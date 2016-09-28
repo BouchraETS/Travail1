e7bdba5a97734258da5c323c7866f000c1d49190
Fanane, Bouchra <an82930@logti-a3412-50>
2016-09-28 17:47:45 -0400

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Fanane, Bouchra on 16-09-28.
+//  Copyright © 2016 Fanane, Bouchra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Fanane, Bouchra on 16-09-28.
+//  Copyright © 2016 Fanane, Bouchra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
